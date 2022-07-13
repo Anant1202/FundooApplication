c1fd0f90746e7f54c4fb3eebc4b8617089030f53
Anant1202 <anantsingh1202@gmail.com>
2022-07-13 16:54:07 +0530

    Repository layer To Business Layer

--- BusinessLayer/Services/UserBL.cs
+++ BusinessLayer/Services/UserBL.cs
@@ -1,10 +1,32 @@
-﻿using System;
+﻿using BusinessLayer.Interface;
+using CommonLayer.Model;
+using RepositoryLayer.Entities;
+using RepositoryLayer.Interface;
+using RepositoryLayer.Services;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace BusinessLayer.Services
 {
-    public class UserBL
+    public class UserBL : IUserBL
     {
+        private readonly IUserRL userRL;
+        public UserBL(IUserRL userRL)
+        {
+            this.userRL = userRL;
+        }
+        public UserEntity Register(UserRegistrationModel userRegistrationModel)
+        {
+            try 
+	        {	        
+                return userRL.Register(userRegistrationModel);
+            }
+	        catch (Exception ex)
+	        {
+                throw ex;
+	        }
+        }
     }
 }
+
