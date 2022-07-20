fe07f725b956469585a4a1fb0637652babf18a96
Anant@1202 <anantsingh1202@gmail.com>
2022-07-19 18:54:22 -0700

    Repository layer to Business layer for Notes

--- BusinessLayer/Services/NotesBL.cs
+++ BusinessLayer/Services/NotesBL.cs
@@ -1,10 +1,31 @@
-﻿using System;
+﻿using BusinessLayer.Interface;
+using CommonLayer.Model;
+using RepositoryLayer.Entities;
+using RepositoryLayer.Interface;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace BusinessLayer.Services
 {
-    public class NotesBL
+    public class NotesBL : INotesBL 
     {
+        private readonly INotesRL notesRL;
+        public NotesBL(INotesRL notesRL)
+        {
+            this.notesRL = notesRL;
+        }
+        public NoteEntity CreateNote(NoteCreateModel noteCreateModel)
+        {
+            try
+            {
+                return notesRL.CreateNote(noteCreateModel);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
     }
 }
